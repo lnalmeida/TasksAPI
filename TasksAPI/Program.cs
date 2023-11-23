283287d0f06b4fd80aaed028b367f2ec756bbf39
Luiz Almeida <luiz.junior-ext@telecall.com>
2023-11-23 09:48:36 -0300

    Adicionar arquivos de projeto.

---
+++ TasksAPI/Program.cs
@@ -0,0 +1,24 @@
+using System.Reflection.Metadata.Ecma335;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "API online");
+
+app.MapGet("/phrases", async () => await new HttpClient().GetStringAsync("https://api.quotable.io/random"));
+
+app.Run();
+
