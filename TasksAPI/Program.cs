3f7ef241f7d363626b830f41e646592280706a6e
Luiz Almeida <luiz.junior-ext@telecall.com>
2023-11-23 10:50:57 -0300

    added: endpoints get tasks, post

--- TasksAPI/Program.cs
+++ TasksAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Reflection.Metadata.Ecma335;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("TasksDB"));
 
 var app = builder.Build();
 
@@ -20,5 +27,39 @@ app.MapGet("/", () => "API online");
 
 app.MapGet("/phrases", async () => await new HttpClient().GetStringAsync("https://api.quotable.io/random"));
 
+app.MapGet("/tasks", async (AppDbContext db) => await db.Tasks.ToListAsync());
+
+app.MapPost("/tasks", async (Task task, AppDbContext db) =>
+{
+    db.Tasks.Add(task);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tasks/{task.Id}", task);
+});
+
+
 app.Run();
+
+class Task
+{
+    public Task() 
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.Now.Date;
+    }   
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; } 
+    [Required(ErrorMessage ="The title can not be empty.")]
+    public string? Title { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public bool IsCompleted { get; set; } = false;
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+    public DbSet<Task> Tasks => Set<Task>();
+}
 
